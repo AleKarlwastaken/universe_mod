18226e93b5d1b52a1c1fbae6e93f12b45cf5795e
AleKarlwastaken <jamce2021@gmail.com>
2023-07-01 16:03:05 +0800

    Initial commit

---
+++ universe-mod-template-1.20.1/src/main/java/alekarl/universe_mod/UniverseModClient.java
@@ -0,0 +1,10 @@
+package alekarl.universe_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UniverseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
